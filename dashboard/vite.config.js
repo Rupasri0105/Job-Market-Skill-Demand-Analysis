5032fdc384a703e85bd97494199274be81ca8b8d
Rupasri0105 <2303a51918@sru.edu.in>
2026-03-16 22:33:50 +0530

    fix base path

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Job-Market-Skill-Demand-Analysis/"
+  base: "/Job-Market-Skill-Demand-Analysis/",
 })
